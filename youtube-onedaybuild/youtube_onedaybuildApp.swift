a6a8e1abd0b1ddb517cd38ddad4ec28ca9073827
Madison <madison.k.adams@gmail.com>
2021-03-29 20:48:52 -0400

    first initial commit

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by Tracy Adams on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
